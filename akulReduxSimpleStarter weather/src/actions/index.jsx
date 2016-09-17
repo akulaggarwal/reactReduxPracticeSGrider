475349df49f1763872e43e4bdfe71096cf6bce99
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-09-16 18:25:22 -0700

    [weather] weather API Get works

--- akulReduxSimpleStarter weather/src/actions/index.jsx
+++ akulReduxSimpleStarter weather/src/actions/index.jsx
@@ -1,11 +1,18 @@
-const API_KEY = '';
+import axios from 'axios';
+
+const API_KEY = 'e05fbc14f8cdae72882efdde6b63eca5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-/*
-export function fetchWeather() {
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url); //returns promise to request due to axios library
+
   return {
-    type: 'FETCH_WEATHER'
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
- */
