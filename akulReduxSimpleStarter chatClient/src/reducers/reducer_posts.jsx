2ce269c66fb8030844ad32f019383f2aea19448f
Akul Aggarwal <akul.aggarwal@gmail.com>
2016-10-08 15:23:06 -0700

    [deploy] change to correct url

--- akulReduxSimpleStarter chatClient/src/reducers/reducer_posts.jsx
+++ akulReduxSimpleStarter chatClient/src/reducers/reducer_posts.jsx
@@ -5,7 +5,7 @@ const INITIAL_STATE = { all: [], post: null};
 export default function(state = INITIAL_STATE, action) {
   switch(action.type) {
     case FETCH_POSTS:
-      console.log('payload.data:', action.payload.data)
+      console.log('payload.data:', action.payload)
       return {...state, all: action.payload.data};
     default:
       return state;
